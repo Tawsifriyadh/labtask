dd3703f7a1728e25f0c83bd1dd327effbb193f95
Tawsifriyadh <tawsif.riyadh.2000@gmail.com>
2020-10-14 21:59:57 +0600

    first commit

---
+++ task1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Hello");
+                Console.WriteLine("Riyadh");
+            }
+
+
+            
+            {
+                int a = 5;
+                int b = 6;
+                int sum = a+b;
+                Console.WriteLine(sum);
+            }
+
+            {
+
+                int a = 10;
+                int b = 2;
+                int div = a / b;
+                Console.WriteLine(div);
+
+
+            }
+            {
+
+                int a = 5;
+                int b = 8;
+                int c = 6;
+                int cal = -a + b * c;
+                Console.WriteLine(cal);
+            }
+            {
+                int a = 55;
+                int b = 9;
+
+                int cal = (a + b) % b;
+                Console.WriteLine(cal);
+            }
+            {
+                int cal = 20 + -3 * 5 / 8;
+                Console.WriteLine(cal);
+            }
+            {
+                int cal = 5 + 15 / 3 * 2 - 8 % 3;
+                Console.WriteLine(cal);
+            }
+            
+            
+            {
+                int a = 12;
+                int b = 2;
+                int sum = a + b;
+                int sub = a - b;
+                int mul = a * b;
+                int div = a / b;
+                int ri = a % b;
+                Console.WriteLine(sum);
+                Console.WriteLine(sub);
+                Console.WriteLine(mul);
+                Console.WriteLine(div);
+                Console.WriteLine(ri);
+            }
+           
+            
+            {
+                double cal = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+                Console.WriteLine(cal);
+            }
+
+            { 
+                double width = 5.5;
+                double height = 8.5;
+                double area = width * height;
+                double perimeter = 2 * (width + height);
+                Console.WriteLine("Area is " + width + "*" + height + " = " + area);
+                Console.WriteLine("Perimeter is 2 * (" + width + "+" + height + ") = " + perimeter);
+            }
+
+
+
+     
